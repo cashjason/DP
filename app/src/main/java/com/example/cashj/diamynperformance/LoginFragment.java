3f09c508d80fd9af4b509994cb51bf3f9d3a81f9
cashjason <cashjason1@gmail.com>
2017-11-13 17:12:40 -0500

    Added files

---
+++ app/src/main/java/com/example/cashj/diamynperformance/LoginFragment.java
@@ -0,0 +1,14 @@
+package com.example.cashj.diamynperformance;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LoginFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
